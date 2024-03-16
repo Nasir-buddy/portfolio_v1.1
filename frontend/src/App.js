c51bcf9fce1f5ad49c2e7daa986dd5776dd2bae5
nasir-buddy <syedali172000@gmail.com>
2024-03-17 03:40:54 +0530

    portfolio: 'setup all the frontend files and file structure'

---
+++ frontend/src/App.js
@@ -0,0 +1,21 @@
+import logo from './logo.svg';
+import './App.css';
+
+import { About, Footer, Header, Skills, Testmonial, Work } from './container'
+// import { Navbar } from './components';
+import Navbar from './components/Navbar/Navbar';
+function App() {
+  return (
+    <div className='app'>
+      <Navbar />
+      <Header />
+      <About />
+      <Skills />
+      <Testmonial />
+      <Work />
+      <Footer />
+    </div>
+  );
+}
+
+export default App;

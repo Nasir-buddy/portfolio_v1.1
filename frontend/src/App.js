948626d297ec09c49fffe094c02b5f58daaba20c
nasir-buddy <syedali172000@gmail.com>
2024-03-17 23:30:46 +0530

    portfolio: add constants and completed the header section

--- frontend/src/App.js
+++ frontend/src/App.js
@@ -4,6 +4,7 @@ import './App.css';
 import { About, Footer, Header, Skills, Testmonial, Work } from './container'
 // import { Navbar } from './components';
 import Navbar from './components/Navbar/Navbar';
+import './App.scss'
 function App() {
   return (
     <div className='app'>

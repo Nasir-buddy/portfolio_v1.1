975d4d90411e997b9f7458c0179ce5f951e87bcf
nasir-buddy <syedali172000@gmail.com>
2024-03-18 03:34:38 +0530

    portfolio: completed about section

--- frontend/src/container/About/About.jsx
+++ frontend/src/container/About/About.jsx
@@ -1,8 +1,45 @@
-import React from 'react'
+import React, { useEffect, useState } from 'react'
 import './About.scss'
+import { motion } from 'framer-motion'
+import { images } from '../../constants'
+import { client, urlFor } from '../../client'
+
+
 const About = () => {
+
+  const [about, setAbout] = useState([]);
+
+  useEffect(() => {
+    client.fetch(`*[_type == "abouts"]`)
+    .then((data) => {
+      setAbout(data);
+    })
+    .catch((error)=>{
+      console.log(error);
+    })
+  }, [])
+
+
+
   return (
-    <div>About</div>
+    <>
+      <h2 style={{marginTop: 50}} className='head-text'>I know that <span> Good Development </span> <br /> means <span> Good Business </span></h2>
+      <div className='app__profiles'>
+        {about.map((item, index) => (
+          <motion.div
+            whileInView={{ opacity: 1 }}
+            whileHover={{ scale: 1.1 }}
+            transition={{ duration: 0.5, type: 'tween' }}
+            className='app__profile-item'
+            key={item.title + index}
+          >
+            <img src={urlFor(item.imgUrl)} alt={item.title} />
+            <h2 className='bold-text' style={{ marginTop: 20 }}>{item.title}</h2>
+            <p className='p-text' style={{ marginTop: 10 }}>{item.description}</p>
+          </motion.div>
+        ))}
+      </div>
+    </>
   )
 }
 

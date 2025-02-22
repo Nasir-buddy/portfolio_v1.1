595185346f92cf8eebaa2498dab8519058cf13e5
Nasir-Buddy <syedali172000@gmail.com>
2025-02-23 01:03:40 +0530

    Style: Update Navbar component to replace logo with text and enhance heading styles

--- frontend/src/components/Navbar/Navbar.jsx
+++ frontend/src/components/Navbar/Navbar.jsx
@@ -10,10 +10,11 @@ const Navbar = () => {
   return (
     <nav className='app__navbar'>
       <div className='app__navbar-logo'>
-        <img src={images.logo} alt="" />
+        {/* <img src={images.logo} alt="" /> */}
+        <h2>dev.<span>Nasir</span></h2>
       </div>
       <ul className='app__navbar-links'>
-        {['home', 'about', 'work', 'skills','testimonial', 'contact'].map((item, index) => (
+        {['home', 'about', 'work', 'skills', 'testimonial', 'contact'].map((item, index) => (
           <li className='app__flex p-text ' key={`link-${item}`}>
             <div />
             <a href={`#${item}`}>{item}</a>
@@ -37,7 +38,7 @@ const Navbar = () => {
             >
               <HiX onClick={() => setToggle(false)} />
               <ul>
-                {['home', 'about', 'work', 'skills','testimonial', 'contact'].map((item, index) => (
+                {['home', 'about', 'work', 'skills', 'testimonial', 'contact'].map((item, index) => (
                   <li key={item}>
                     <a href={`#${item}`} onClick={() => setToggle(false)}>{item}</a>
                   </li>

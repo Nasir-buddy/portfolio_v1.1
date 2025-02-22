cb945ac589e102d8837d65d05d0f917095e702d0
Nasir-Buddy <syedali172000@gmail.com>
2025-02-23 00:40:46 +0530

    Refactor Skills component layout and styles for improved responsiveness and readability

--- frontend/src/container/Skills/Skills.jsx
+++ frontend/src/container/Skills/Skills.jsx
@@ -33,30 +33,13 @@ const Skills = () => {
   return (
     <>
       <h2 className='head-text'
-      style={{ marginTop: 20 }}
+        style={{ marginTop: 20 }}
       >
         <span>Skills</span> & Experience
       </h2>
 
       <div className='app__skills-container'>
-        <motion.div className='app__skills-list'>
-          {skills?.map((skill, index) => (
-            <motion.div
-              whileInView={{ opacity: [0, 1] }}
-              transition={{ duration: 0.5 }}
-              className='app__skills-item app__flex'
-              key={skill.name}
-              
-            >
-              <div className='app__flex' style={{ backgroundColor: skill.bgColor }}>
-                <img src={urlFor(skill.icon)} alt={skill.name} />
-              </div>
-              <p className='p-text'>
-                {skill.name}
-              </p>
-            </motion.div>
-          ))}
-        </motion.div>
+
 
 
         <motion.div
@@ -95,14 +78,31 @@ const Skills = () => {
           ))}
 
         </motion.div>
+        <motion.div className='app__skills-list'>
+          {skills?.map((skill, index) => (
+            <motion.div
+              whileInView={{ opacity: [0, 1] }}
+              transition={{ duration: 0.5 }}
+              className='app__skills-item app__flex'
+              key={skill.name}
 
+            >
+              <div className='app__flex' style={{ backgroundColor: skill.bgColor }}>
+                <img src={urlFor(skill.icon)} alt={skill.name} />
+              </div>
+              <p className='p-text'>
+                {skill.name}
+              </p>
+            </motion.div>
+          ))}
+        </motion.div>
       </div>
     </>
   )
 }
 
 export default AppWrap(
-  MotionWrap(Skills, 'app__skills'), 
-  'skills', 
+  MotionWrap(Skills, 'app__skills'),
+  'skills',
   'app__whitebg'
 );

a5c3c7a8a8ef0bd8b2ae72e71775496617002528
nasir-buddy <syedali172000@gmail.com>
2025-05-19 23:14:45 +0530

    update: resume april 19

--- frontend/src/container/Header/Header.jsx
+++ frontend/src/container/Header/Header.jsx
@@ -18,7 +18,7 @@ const Header = () => {
     const handleDownloadResume = () => {
         try {
             if (window.confirm("Are you sure you want to download rhe Updated Resume?")) {
-                window.open('https://drive.google.com/file/d/1-GBqilZM-HgnVlI0uwW63CwQo0LrI3wr/view?usp=drive_link', '_blank')
+                window.open('https://drive.google.com/file/d/14JGWRAhouKwrzE8rRNkFw6MLZmcrsKO9/view?usp=drive_link', '_blank')
             }
         } catch (error) {
             console.log("Failed to download rhe Updated Resume.", error);

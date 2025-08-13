119163a58acd190e3452833aa1d9a5e9de84267c
Nasir-Ali <syedali172000@gmail.com>
2025-08-13 21:44:47 +0530

    Update Header.jsx

--- frontend/src/container/Header/Header.jsx
+++ frontend/src/container/Header/Header.jsx
@@ -18,7 +18,7 @@ const Header = () => {
     const handleDownloadResume = () => {
         try {
             if (window.confirm("Are you sure you want to download rhe Updated Resume?")) {
-                window.open('https://drive.google.com/file/d/1yJsJi_VG53Hvhw9h_pLfrXjOwlAl_pGr/view?usp=drive_link', '_blank')
+                window.open('https://drive.usercontent.google.com/u/0/uc?id=1eai3BfHYtNVln1p_EyDnWppNRYDmkNiF&export=download', '_blank')
             }
         } catch (error) {
             console.log("Failed to download rhe Updated Resume.", error);
